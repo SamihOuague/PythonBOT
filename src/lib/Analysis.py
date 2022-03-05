f566dde66363a5f54f175af08bb978aba725cf82
Samih Ouague <ouaguenouni.samih@gmail.com>
2022-03-05 06:02:26 +0100

    Analysis class OK

---
+++ src/lib/Analysis.py
@@ -0,0 +1,34 @@
+class Analysis:
+    def __init__(self, candles):
+        self.candles = candles
+
+    def getCandle(self, pos):
+        return self.candles[pos]
+
+    def mobileAverage(self, period = 7, pos = 0):
+        candles = self.candles[pos:(pos + period)]
+        sum = 0
+        for c in candles:
+            sum += float(c[4])
+        return round(sum/period, 4)
+
+    def getRSI(self, period = 14, pos = 0):
+        candles = self.candles[pos:(pos + period)]
+        avgH = []
+        avgL = []
+
+        for i in range(1, len(candles)):
+            current = candles[i]
+            prev = candles[i - 1]
+            diff = float(prev[4]) - float(current[4])
+            if diff < 0:
+                avgL.append(-1 * float(diff))
+                avgH.append(0)
+            else:
+                avgH.append(float(diff))
+                avgL.append(0)
+
+        avgH = sum(avgH)/len(avgH)
+        avgL = sum(avgL)/len(avgL)
+
+        return round((avgH/(avgL + avgH)) * 100)
